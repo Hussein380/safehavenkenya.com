f83dd17b84c16a63b71bfb6d75e4820f7c328c7f
unknown <huznigarane@gmail.com>
2025-12-13 20:59:33 +0300

    m

--- api/index.ts
+++
@@ -1,3 +0,0 @@
-import app from '../apps/backend/src/app';
-
-export default app;
--- apps/backend/src/routes/mentalHealthRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { analyzeMentalState } from '../controllers/mentalHealthController';
-
-const router = Router();
-
-router.post('/analyze', analyzeMentalState);
-
-export default router;
